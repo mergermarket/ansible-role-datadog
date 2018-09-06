6fcda01f4afbdf4c3e3566a8f3e31736aa4776aa
Arthur Gassner <arthur.gassner@gmail.com>
2018-09-06 15:29:18 +0100

    Add lvs full path

--- files/datadog-docker-disk-usage.py
+++ files/datadog-docker-disk-usage.py
@@ -11,8 +11,8 @@ options = {
 
 initialize(**options)
 
-data_percent = subprocess.check_output(["lvs", "--no-headings", "-o", "data_percent"])
-metadata_percent = subprocess.check_output(["lvs", "--no-headings", "-o", "metadata_percent"])
+data_percent = subprocess.check_output(["/sbin/lvs", "--no-headings", "-o", "data_percent"])
+metadata_percent = subprocess.check_output(["/sbin/lvs", "--no-headings", "-o", "metadata_percent"])
 
 statsd.gauge('mmdocker.thinpool.data.in_use', float(data_percent))
 statsd.gauge('mmdocker.thinpool.metadata.in_use', float(metadata_percent))
